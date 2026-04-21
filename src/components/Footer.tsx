c32f09696b2873d5ef0a66b8441946822592fcd9
Gabriel Yuji Yasuda Cardoso <gabrielyyasuda@gmail.com>
2026-04-21 15:14:06 -0300

    feat: update Footer component to use constants for GitHub and LinkedIn URLs

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,5 @@
+import { GLOBALS } from "../shared/constants";
+
 const Footer = () => {
   return (
     <footer className="footer sm:footer-horizontal text-[#6c6c75] items-center p-7 border-t border-base-300 px-[25%]">
@@ -5,9 +7,12 @@ const Footer = () => {
         <p>Copyright © {new Date().getFullYear()} Gabriel Yuji - All right reserved</p>
       </aside>
       <nav className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
-        <a>GitHub</a>
-        <a>Linkedin</a>
-        <a>Email</a>
+        <a href={GLOBALS.GITHUB_URL} target="_blank" rel="noopener noreferrer">
+          GitHub
+        </a>
+        <a href={GLOBALS.LINKEDIN_URL} target="_blank" rel="noopener noreferrer">
+          Linkedin
+        </a>
       </nav>
     </footer>
   );
